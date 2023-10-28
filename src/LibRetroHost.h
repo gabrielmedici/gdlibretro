fe61d47c832635a5bbc1921c8c409ab0c4c70166
Gabriel Medici <j.gabriel.medici@gmail.com>
2023-10-28 01:43:30 -0300

    Now runs 1 cycle, YAY This was much more diffucult than I thought it would be.  - Added a fuck ton of funcions that the libretro core calls us on  - Copy and pasted the environment setup thing, and spent 5 hours trying to figure out why the program was crashing with a division by zero (it was because i wasn't setting the sample rate on RETRO_ENVIRONMENT_GET_VARIABLE) Cant remember more

--- src/LibRetroHost.h
+++ src/LibRetroHost.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "godot_cpp/classes/object.hpp"
+#include "libretro.h"
 #include "Windows.h"
 
 namespace godot
@@ -25,12 +26,12 @@ static struct {
   // bool retro_unserialize(const void *data, size_t size);
   // void retro_cheat_reset(void);
   // void retro_cheat_set(unsigned index, bool enabled, const char *code);
-//   bool (*retro_load_game)(const struct retro_game_info* game);
+  bool (*retro_load_game)(const struct retro_game_info* game);
   // bool retro_load_game_special(
   //   unsigned game_type,
   //   const struct retro_game_info *info,
   //   size_t num_info);
-//   void (*retro_unload_game)(void);
+  void (*retro_unload_game)(void);
   // unsigned retro_get_region(void);
   // void *retro_get_memory_data(unsigned id);
   // size_t retro_get_memory_size(unsigned id);
@@ -46,7 +47,23 @@ class LibRetroHost : public godot::Object
 public:
     static bool load_core(godot::String path);
     static void unload_core();
+    static unsigned width;
+    static unsigned height;
 
 private:
     static void _bind_methods();
-};
+
+    static bool core_environment(unsigned cmd, void *data);
+    static void core_video_refresh(const void *data, unsigned width, unsigned height, size_t pitch);
+    static void core_input_poll(void);
+    static int16_t core_input_state(unsigned port, unsigned device, unsigned index, unsigned id);
+    static void core_audio_sample(int16_t left, int16_t right);
+    static size_t core_audio_sample_batch(const int16_t *data, size_t frames);
+    static bool core_load_game(const char * filename);
+
+    static void video_configure(const struct retro_game_geometry * geom);
+    static void video_deinit();
+    static void audio_init(retro_system_av_info av);
+    static struct retro_audio_callback audio_callback;
+    static void audio_deinit();
+};  
