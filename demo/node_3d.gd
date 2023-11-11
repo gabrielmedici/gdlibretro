64ea840392b2c5955c2aa592357366d12f77a2c9
Gabriel Medici <j.gabriel.medici@gmail.com>
2023-11-11 19:31:19 -0300

    Added some obvious comments to node_3d.gd

--- demo/node_3d.gd
+++ demo/node_3d.gd
@@ -1,22 +1,27 @@
 extends Node3D
 
 @onready var tex_rect = get_node("TextureRect")
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	# Loads the core, the argument is the dll name inside the libretro-cores folder. WITHOUT THE .DLL
 	RetroHost.load_core("dosbox_pure_libretro")
+
 	#RetroHost.load_core("vice_x64sc_libretro")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# Run one iteration of the core
 	RetroHost.run()
+
+	# Retrieve the framebuffer image
 	var frame_buffer = RetroHost.get_frame_buffer()
 	if(!frame_buffer):
 		return
-		
+	
+	# Fuckery to display the image
 	var img_tex = ImageTexture.create_from_image(frame_buffer)
 	$MeshInstance3D.get_surface_override_material(0).albedo_texture = img_tex
 	return
 	
 func _input(event):
+	# Forward input events to the frontend  (duh)
 	RetroHost.forward_input(event)
